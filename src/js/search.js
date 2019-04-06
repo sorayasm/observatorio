23d2eeaa09dcdf6d3ae7faff59074483cfd17847
Soraya SM <soraya.sacaan@gmail.com>
2019-04-05 23:46:44 -0300

    search

--- src/js/search.js
+++ src/js/search.js
@@ -1,35 +1,18 @@
-(function() {
-    var data =  [
-        {
-            title: "Esteban",
-            content: 43,
-            link: "male"
-        } , {
-            title: "Zara",
-            content: 65,
-            link:  "female"
-        }, {
-            title: "Esteban Javier",
-            content: 43,
-            link: "male"
-        } 
-        ];
+window.onload = function() {
+    var query = window.location.search.substring(1).split("=")[1].toLowerCase();
+    var searchRes = document.getElementById("search-res");
+    searchRes.innerHTML = "<h4> Resultados para " + query + ":</h4>";
+    $(function () {
+        $('#myInput').val(query);
+      });
+}
 
-        results = [];
-        for (i = 0; i < data.length; i++){
-        var query = window.location.search.substring(1).split("=")[1].toLowerCase();
-        var searchRes = document.getElementById("search-res");
-        searchRes.innerHTML = "<h4> Resultados para " + query + ":</h4>";
-        var printRes = document.getElementById("print-res");
-
-        if (!data[i].title.toLowerCase().includes(query)) {
-            console.log("else " + data[i].title)
-            printRes.innerHTML = "<p>Lo sentimos no hay resultados para tu búsqueda.</p>";
-        } else {
-            console.log( "list " +data[i].title)
-            printRes.innerHTML ="<h5>" + data[i].title + "</h5><br><p>" + data[i].content + "</p><br><a src='" + data[i].link+ "'>" + data[i].link+"</a>";
-        }
-    }
-        
-    })();
+$(document).ready(function(){
+    $("#myInput") .on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myList li").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+    });
 
